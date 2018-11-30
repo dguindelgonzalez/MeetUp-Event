334a7d9aa390d654af1b55358a025f626d7ca290
D. Guindel González <d.guindel.gonzalez@avanade.com>
2018-11-30 15:10:44 +0100

    First Release Cain

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -10,14 +10,17 @@ public class MainSceneManager : MonoBehaviour
 {
 
     #region Private Fields
+
     private static ApplicationStateType applicationState;
     private const string DirectLineKey = "yPS_3_qNtp8.cwA.Doo.pIyO5bOUHHCEJGGTjfpK-0oFXCtpyIUDpaLG_EKG4nI";
     private DictationRecognizer dictationRecognizer;
     private TextToSpeech textToSpeech;
     private string conversationId;
+
     #endregion
 
     #region Public Properties
+
     public static MainSceneManager instance;
     public Material Traza01Mat;
     public Material Traza02Mat;
@@ -28,7 +31,7 @@ public class MainSceneManager : MonoBehaviour
     {
         IdleState = 0,
         BotState = 1,
-        VisionState = 2,
+        VisionState = 2
     }
 
     public static ApplicationStateType ApplicationState
@@ -40,9 +43,11 @@ public class MainSceneManager : MonoBehaviour
             ChangeView(value);
         }
     }
+
     #endregion
 
     #region Unity Defaults Methods
+
     void Awake()
     {
         // allows this instance to behave like a singleton
@@ -51,12 +56,6 @@ public class MainSceneManager : MonoBehaviour
 
         textToSpeech = gameObject.AddComponent<TextToSpeech>();
         textToSpeech.Voice = TextToSpeechVoice.Mark;
-
-        dictationRecognizer = new DictationRecognizer();
-        dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
-        dictationRecognizer.DictationHypothesis += DictationRecognizer_DictationHypothesis;
-        dictationRecognizer.DictationComplete += DictationRecognizer_DictationComplete;
-        dictationRecognizer.DictationError += DictationRecognizer_DictationError;
     }
 
     void Update()
@@ -76,6 +75,8 @@ public class MainSceneManager : MonoBehaviour
     #region Public Methods
     public void OnVoiceCommand(string command)
     {
+        Debug.Log(command);
+
         switch (command)
         {
             case "Bot":
@@ -90,11 +91,6 @@ public class MainSceneManager : MonoBehaviour
 
                 ApplicationState = ApplicationStateType.BotState;
 
-                if (dictationRecognizer.Status != SpeechSystemStatus.Stopped)
-                {
-                    dictationRecognizer.Stop();
-                }
-
                 break;
 
             case "Vision":
@@ -102,11 +98,6 @@ public class MainSceneManager : MonoBehaviour
                 {
                     SceneAnimatorController.SetTrigger("IronVisionState");
                     ApplicationState = ApplicationStateType.VisionState;
-
-                    if (dictationRecognizer.Status != SpeechSystemStatus.Stopped)
-                    {
-                        dictationRecognizer.Stop();
-                    }
                 }
 
                 break;
@@ -115,6 +106,12 @@ public class MainSceneManager : MonoBehaviour
                 if (applicationState == ApplicationStateType.BotState)
                 {
                     PhraseRecognitionSystem.Shutdown();
+
+                    dictationRecognizer = new DictationRecognizer();
+                    dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+                    dictationRecognizer.DictationHypothesis += DictationRecognizer_DictationHypothesis;
+                    dictationRecognizer.DictationComplete += DictationRecognizer_DictationComplete;
+                    dictationRecognizer.DictationError += DictationRecognizer_DictationError;
 
                     dictationRecognizer.Start();
                 }
@@ -126,9 +123,11 @@ public class MainSceneManager : MonoBehaviour
                 break;
         }
     }
+
     #endregion
 
     #region Private Methods
+
     private static void ChangeView(ApplicationStateType value)
     {
         switch (value)
@@ -176,6 +175,22 @@ public class MainSceneManager : MonoBehaviour
                     var messageActivity = e.Messages.LastOrDefault();
                     Debug.Log(messageActivity.Text);
                     textToSpeech.SpeakSsml("<?xml version=\"1.0\"?><speak speed=\"80%\" version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.w3.org/2001/10/synthesis http://www.w3.org/TR/speech-synthesis/synthesis.xsd\" xml:lang=\"en-US\"> " + messageActivity.Text + "</speak>");
+
+                    dictationRecognizer.Stop();
+
+                    dictationRecognizer.DictationResult -= DictationRecognizer_DictationResult;
+                    dictationRecognizer.DictationHypothesis -= DictationRecognizer_DictationHypothesis;
+                    dictationRecognizer.DictationComplete -= DictationRecognizer_DictationComplete;
+                    dictationRecognizer.DictationError -= DictationRecognizer_DictationError;
+
+                    dictationRecognizer.Dispose();
+
+                    //do
+                    //{
+
+                    //} while (dictationRecognizer.Status != SpeechSystemStatus.Stopped);
+
+                    PhraseRecognitionSystem.Restart();
                 }
                 break;
             case EventTypes.Error:
